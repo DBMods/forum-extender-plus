4f8fa8d0c9f474c7eb851d6bc52829d8b97f7042
Andrew Yasger <techgeek01help@gmail.com>
2013-05-23 14:45:54 -0500

    Update dropbox_forum_mod_icons.user.js

--- dropbox_forum_mod_icons.user.js
+++ dropbox_forum_mod_icons.user.js
@@ -6,3 +6,18 @@
 // @require //ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js
 // @grant none
 // ==/UserScript==
+
+var internalVersion = 2013.1;
+
+//Run necessary functions and checks
+checkPage("threadauthor");
+
+//Check page for posts by Super Users
+function checkPage(matchClass){
+  var elems = document.getElementById("*"),i;
+  for (i in elems){
+    if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1){
+      elems[i].getElementsByTagName("p").getElementsByTagName("strong").write("<img src='http://www.techgeek01.com/just-for-fun/nyancatright.gif' height='16px' width='40px' />");
+    }
+  }
+};
