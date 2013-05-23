3ea860bfb286e9600285bde4989a58f205594b76
Andrew Yasger <techgeek01help@gmail.com>
2013-05-23 14:10:23 -0500

    Create dropbox_forum_mod_icons.user.js
    Initial Greasemonkey framework

---
+++ dropbox_forum_mod_icons.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Dropbox Forum Mod Icons
+// @namespace IdeativeSoftware.Dropbox
+// @description Gives Dropbox Forum Super Users icons
+// @include https://forums.dropbox.com/*
+// @require //ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js
+// @grant none
+// ==/UserScript==
