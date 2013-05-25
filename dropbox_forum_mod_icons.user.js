287058a902f532e4ee20db5e34984f571de20892
unknown <techgeek01help@gmail.com>
2013-05-25 14:36:37 -0500

    Icons restricted to Super Users only

--- dropbox_forum_mod_icons.user.js
+++ dropbox_forum_mod_icons.user.js
@@ -6,17 +6,25 @@
 // @grant none
 // ==/UserScript==
 
-var internalVersion = 2013.1;
+var internalVersion = 2013.5;
 
-//Run necessary functions and checks
+//Add footer
+addFooter();
+
+//Run necessary checks and changes
 checkPage("threadauthor");
 
 //Check page for posts by Super Users
 function checkPage(matchClass){
   var elems = document.getElementsByTagName('*'),i;
   for (i in elems){
-    if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1){
+    if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1 && elems[i].innerHTML.indexOf("</a></small>") - 10 == elems[i].innerHTML.indexOf("Super User")){
       elems[i].innerHTML = "<p><strong><img src='http://www.techgeek01.com/just-for-fun/nyancatright.gif' height='16px' width='40px' >&nbsp;" + elems[i].innerHTML.substring(26,elems[i].innerHTML.length);
     }
   }
+}
+
+//Append footer
+function addFooter(){
+  document.getElementById("footer").innerHTML = document.getElementById("footer").innerHTML + "<div style='text-align: right; color: rgb(102, 102, 102); font-size: 11px; clear:both;'>Dropbox Forum Mod Icons Version " + internalVersion + "</div>";
 }
