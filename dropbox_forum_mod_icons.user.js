9b9220bddb2ba90773322a08f44cbb591676fd85
unknown <techgeek01help@gmail.com>
2013-05-25 22:27:43 -0500

    Update to 2013.6

--- dropbox_forum_mod_icons.user.js
+++ dropbox_forum_mod_icons.user.js
@@ -6,20 +6,50 @@
 // @grant none
 // ==/UserScript==
 
-var internalVersion = 2013.5;
+//Set internal version
+var internalVersion = 2013.6;
+
+//Set all required variables
+var elems = document.getElementsByTagName("*"), i;
 
 //Add footer
 addFooter();
 
 //Run necessary checks and changes
-checkPage("threadauthor");
+changePage("author", "role", "Super User", "<img src='http://www.techgeek01.com/just-for-fun/nyancatright.gif' height='16px' width='40px' >&nbsp;");
 
-//Check page for posts by Super Users
-function checkPage(matchClass){
-  var elems = document.getElementsByTagName('*'),i;
-  for (i in elems){
-    if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1 && elems[i].innerHTML.indexOf("</a></small>") - 10 == elems[i].innerHTML.indexOf("Super User")){
-      elems[i].innerHTML = "<p><strong><img src='http://www.techgeek01.com/just-for-fun/nyancatright.gif' height='16px' width='40px' >&nbsp;" + elems[i].innerHTML.substring(26,elems[i].innerHTML.length);
+//Manipulate page
+function changePage(checkPart, checkFor, checkForSubClass, metaData){
+  //Process and direct page check
+  if (checkPart == "author"){
+    if (checkFor == "role"){
+      var authorIndex = "</a></small>";
+    } else if (checkFor == "id"){
+      var authorIndex = checkForSubClass;
+      var checkForSubclass = '<small><a href="https://forums.dropbox.com/profile.php?id=';
+    }
+    checkAuthor("threadauthor", checkForSubClass, authorIndex, metaData);
+  }
+
+  //Check author information
+  function checkAuthor(matchClass, indexA, indexB, contentAddition){
+    for (i in elems){
+      if ((" " + elems[i].className + " ").indexOf(" " + matchClass + " ") > -1 && elems[i].innerHTML.indexOf(indexB) - indexA.length == elems[i].innerHTML.indexOf(indexA)){
+        elems[i].innerHTML = "<p><strong>" + contentAddition + elems[i].innerHTML.substring(26, elems[i].innerHTML.length);
+        var authorPar = elems[i].getElementsByTagName("p");
+        var authorStrong = authorPar[0].getElementsByTagName("strong");
+        var authorName = authorStrong[0].innerHTML;
+        if (authorName.indexOf("<a href") > -1){
+          authorName = authorStrong[0].getElementsByTagName("a");
+          authorName = authorName[0].innerHTML;
+        }
+        var authorSmall = authorPar[0].getElementsByTagName("small");
+        var authorProfile = authorSmall[0].getElementsByTagName("a");
+        var authorRole = authorProfile[0].innerHTML;
+        if (authorRole == "Super User" && authorName == "Andy Y."){
+          authorProfile[0].innerHTML = "Supreme ruler and guardian of all that he sees and everything he touches turns to gold if he so pleases! Master of Chuck Norris and awesomist of awesome. Also king. All hail the king!";
+        }
+      }
     }
   }
 }
