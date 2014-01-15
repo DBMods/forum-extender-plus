2a6902e4dd6685f9863755a13d1be9446ab019c3
TechGeek01 <techgeek01help@gmail.com>
2014-01-15 17:02:25 -0600

    Replies work now

--- messaging/compose-message.php
+++ messaging/compose-message.php
@@ -23,7 +23,10 @@
 			<?php
 			require 'db-login.php';
 			echo '<p><form action="check-message.php" method="post"><input type="hidden" name="to" value="' . $_POST['from'] . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><button type="submit">Cancel</button></form></p>';
-			echo '<p class="topline"></p>';
+			echo '<div class="topline">';
+			echo '<p></p>';
+			echo '<p><form action="process-message.php" method="post"><input name="referrer" type="hidden" value="compose" /><input name="msgto" type="textbox" placeholder="Recipient" value="' . $_POST['to'] . '"/><input name="msgfrom" type="hidden" value = "' . $_POST['from'] . '"/><textarea name="msgtext" placeholder="Message"></textarea><button type="submit">Send</button></form></p>';
+			echo '</div>';
 			mysql_close($db);
 			?>
 		</div>
--- messaging/process-message.php
+++ messaging/process-message.php
@@ -2,5 +2,9 @@
 require 'db-login.php';
 $result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '")');
 mysql_close($db);
-header('Location: ' . $_POST['returnto']);
+if ($_POST['referrer'] && $_POST['referrer'] == 'compose') {
+	echo 'Message sent';
+} else {
+	header('Location: ' . $_POST['returnto']);
+}
 ?>
