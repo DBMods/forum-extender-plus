e62a56ee00674850781fdb9396b63588d68c8b77
TechGeek01 <techgeek01help@gmail.com>
2014-01-16 15:50:22 -0600

    Added timestamp

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,4 +1,4 @@
 <?php
-$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '")');
+$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Y-m-d') . '")');
 echo '<p>Message sent.</p>';
 ?>
