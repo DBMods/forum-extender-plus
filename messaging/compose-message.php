03a8d03ce7b8c67d1ec50e858f6e8b52f2f52f6d
TechGeek01 <techgeek01help@gmail.com>
2014-01-15 18:02:04 -0600

    Merging send code

--- messaging/compose-message.php
+++ messaging/compose-message.php
@@ -2,6 +2,6 @@
 echo '<p><form action="messages.php" method="post"><input type="hidden" name="to" value="' . $_POST['from'] . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><button type="submit">Cancel</button></form></p>';
 echo '<div class="topline">';
 echo '<p></p>';
-echo '<p><form action="process-message.php" method="post"><input name="referrer" type="hidden" value="compose" /><input name="msgto" type="textbox" placeholder="Recipient" value="' . $_POST['to'] . '"/><input name="msgfrom" type="hidden" value = "' . $_POST['from'] . '"/><textarea name="msgtext" placeholder="Message"></textarea><button type="submit">Send</button></form></p>';
+echo '<p><form action="messages.php" method="post"><input name="action" type="hidden" value="send" /><input name="msgto" type="textbox" placeholder="Recipient" value="' . $_POST['to'] . '"/><input name="msgfrom" type="hidden" value = "' . $_POST['from'] . '"/><textarea name="msgtext" placeholder="Message"></textarea><button type="submit">Send</button></form></p>';
 echo '</div>';
 ?>
--- messaging/messages.php
+++ messaging/messages.php
@@ -22,16 +22,23 @@
 		<div id='wrapper'>
 			<?php
 			require 'db-login.php';
-			if ($_POST['action'] == 'delete')
+			$action = $_POST['action'];
+			if ($action == 'delete')
 				include 'delete-message.php';
-			else if ($_POST['action'] == 'compose')
+			else if ($action == 'compose')
 				include 'compose-message.php';
-			$result = mysql_query("SELECT * FROM `msglist` WHERE `to` = '" . $_POST['to'] . "'");
+			else if ($action == 'send') {
+				$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '")');
+				echo 'Message sent';
+			}
 			echo '<p><a href="' . $_POST['returnto'] . '">Back to forums</a></p>';
-			while ($row = mysql_fetch_assoc($result)) {
-				echo '<p class="topline"><form method="post" action="messages.php"><input type="hidden" name="action" value="delete" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="to" type="hidden" value="' . $_POST['to'] . '" /><input type="hidden" name="from" value="' . $row['from'] . '" /><input type="hidden" name="msg" value="' . stripslashes($row['msg']) . '" /><button type="submit">Delete</button></form>';
-				echo '<form method="post" action="messages.php"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="to" type="hidden" value="' . $row['from'] . '" /><input type="hidden" name="from" value="' . $_POST['to'] . '" /><button type="submit">Reply</button></form>';
-				echo 'From: <a href="https://forums.dropbox.com/profile.php?id=' . $row['from'] . '" target="_blank">' . $row['from'] . '</a><br>Message:<br>' . htmlspecialchars_decode(stripslashes($row['msg'])) . '</p>';
+			if ($_POST['for']) {
+				$result = mysql_query("SELECT * FROM `msglist` WHERE `to` = '" . $_POST['for'] . "'");
+				while ($row = mysql_fetch_assoc($result)) {
+					echo '<p class="topline"><form method="post" action="messages.php"><input type="hidden" name="action" value="delete" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="to" type="hidden" value="' . $_POST['for'] . '" /><input type="hidden" name="from" value="' . $row['from'] . '" /><input type="hidden" name="msg" value="' . stripslashes($row['msg']) . '" /><button type="submit">Delete</button></form>';
+					echo '<form method="post" action="messages.php"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="to" type="hidden" value="' . $row['from'] . '" /><input type="hidden" name="from" value="' . $_POST['for'] . '" /><button type="submit">Reply</button></form>';
+					echo 'From: <a href="https://forums.dropbox.com/profile.php?id=' . $row['from'] . '" target="_blank">' . $row['from'] . '</a><br>Message:<br>' . htmlspecialchars_decode(stripslashes($row['msg'])) . '</p>';
+				}
 			}
 			mysql_close($db);
 			?>
