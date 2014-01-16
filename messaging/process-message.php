a8cb03bcc6197cf522a501dad6d23fc195a0c9c5
TechGeek01 <techgeek01help@gmail.com>
2014-01-16 16:02:46 -0600

    Added timestamp

--- messaging/process-message.php
+++ messaging/process-message.php
@@ -1,6 +1,6 @@
 <?php
 require 'db-login.php';
-$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '")');
+$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Y-m-d') . '")');
 mysql_close($db);
 header('Location: ' . $_POST['returnto']);
 ?>
