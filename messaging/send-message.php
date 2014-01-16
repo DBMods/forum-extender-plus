892b576e8ae520e22a9276a1f0b1c929856fd1e0
TechGeek01 <techgeek01help@gmail.com>
2014-01-15 19:12:24 -0600

    Aesthetic bugfix

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,4 +1,4 @@
 <?php
 $result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '")');
-echo '<p>Message sent.<br><form method="post" action="messages.php"><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input type="hidden" name="for" value="' . $_POST['msgfrom'] . '" /><button type="submit">Back to messages</button></form></p>"';
+echo '<p>Message sent.</p>';
 ?>
