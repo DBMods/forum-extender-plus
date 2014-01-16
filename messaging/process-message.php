b08b78655419a0a7c3d4a57148aee7429470ab71
TechGeek01 <techgeek01help@gmail.com>
2014-01-16 16:15:37 -0600

    Better timestamp

--- messaging/process-message.php
+++ messaging/process-message.php
@@ -1,6 +1,6 @@
 <?php
 require 'db-login.php';
-$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Ymdhms') . '")');
+$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Y-m-d h:m:s') . '")');
 mysql_close($db);
 header('Location: ' . $_POST['returnto']);
 ?>
--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,4 +1,4 @@
 <?php
-$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Ymdhms') . '")');
+$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . date('Y-m-d h:m:s') . '")');
 echo '<p>Message sent.</p>';
 ?>
