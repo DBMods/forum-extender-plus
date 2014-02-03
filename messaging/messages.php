b49f6882e0422939af942ca137ed695a36984577
TechGeek01 <techgeek01help@gmail.com>
2014-02-03 16:55:07 -0600

    Minified code and did some aesthetic things

--- messaging/messages.php
+++ messaging/messages.php
@@ -5,16 +5,19 @@
 			body {
 				background: #ddd;
 				margin: 0;
-				padding: 0;
+				padding: 0
 			}
 			.topline {
-				border-top: 1px solid #bbb;
+				border-top: 1px solid #bbb
 			}
 			#wrapper {
 				width: 900px;
 				background: #fff;
 				margin: auto;
-				padding: 30px 50px;
+				padding: 30px 50px
+			}
+			form.menu {
+				display:inline-block
 			}
 		</style>
 	</head>
@@ -32,12 +35,14 @@
 			$timeOffsetSeconds = $_POST['timeOffset'] * 60;
 			echo '<p><a href="' . $_POST['returnto'] . '">Back to forums</a></p>';
 			if ($_POST['for']) {
-				echo '<p><form action="messages.php" method="post"><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value=""/><input name="to" type="hidden" value="" /><input type="hidden" name="from" value="' . $_POST['for'] . '" /><button type="submit">Compose</button></form></p>';
+				echo '<p class="topline"><form action="messages.php" method="post" class="menu"><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value=""/><input name="to" type="hidden" value="" /><input type="hidden" name="from" value="' . $_POST['for'] . '" /><button type="submit">Compose</button></form></p>';
 				$result = mysql_query("SELECT * FROM `msglist` WHERE `to` = '" . $_POST['for'] . "' ORDER BY `time` DESC");
 				while ($row = mysql_fetch_assoc($result)) {
-					echo '<p class="topline"><form method="post" action="messages.php"><input type="hidden" name="action" value="delete" /><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="time" type="hidden" value="' . $row['time'] . '" /><input name="for" type="hidden" value="' . $_POST['for'] . '" /><input type="hidden" name="from" value="' . $row['from'] . '" /><input type="hidden" name="msg" value="' . stripslashes($row['msg']) . '" /><button type="submit">Delete</button></form>';
-					echo '<form method="post" action="messages.php"><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value="' . stripslashes($row['msg']) . '"/><input name="to" type="hidden" value="' . $row['from'] . '" /><input type="hidden" name="from" value="' . $_POST['for'] . '" /><button type="submit">Reply</button></form>';
-					echo 'Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: <a href="https://forums.dropbox.com/profile.php?id=' . $row['from'] . '" target="_blank">' . $row['from'] . '</a><br>Message:<br>' . htmlspecialchars_decode(stripslashes($row['msg'])) . '</p>';
+					echo '<p class="topline">';
+					echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="action" value="delete" /><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="time" type="hidden" value="' . $row['time'] . '" /><input name="for" type="hidden" value="' . $_POST['for'] . '" /><input type="hidden" name="from" value="' . $row['from'] . '" /><input type="hidden" name="msg" value="' . stripslashes($row['msg']) . '" /><button type="submit">Delete</button></form>';
+					echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value="' . stripslashes($row['msg']) . '"/><input name="to" type="hidden" value="' . $row['from'] . '" /><input type="hidden" name="from" value="' . $_POST['for'] . '" /><button type="submit">Reply</button></form>';
+					echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: <a href="https://forums.dropbox.com/profile.php?id=' . $row['from'] . '" target="_blank">' . $row['from'] . '</a><br>Message:<br>' . htmlspecialchars_decode(stripslashes($row['msg']));
+					echo '</p>';
 				}
 			}
 			mysql_close($db);
