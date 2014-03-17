0eecd908eb0fb3f0ee87db86f1d54dc3eb6aa2d5
Nathan Cheek <myself@nathancheek.com>
2014-03-17 15:18:00 -0400

    Fix for htmlspecialchars

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,7 +1,7 @@
 <?php
 $dest = $_POST['msgto'];
 if (preg_replace('/[\D]/', '', $dest) == $dest && $dest != '') {
-	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($dest) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string(htmlspecialchars($_POST['msgtext'])) . '", "' . time() . '")');
+	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($dest) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
 	echo '<p>Message sent.</p>';
 } else
 	echo '<p>Sorry, invalid destination.<br><form class="menu" action="messages.php" method="post"><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><br><input name="from" type="hidden" value = "' . $_POST['for'] . '"/><input type="hidden" name="msgtext" value="' . $_POST['msgtext'] . '" /><br><button type="submit">Go Back</button></form></p>'
--- messaging/show-sent.php
+++ messaging/show-sent.php
@@ -1,9 +1,9 @@
 <?php
-echo '<p><form action="messages.php" method="post"><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input type="hidden" name="for" value="' . $_POST['from'] . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><button type="submit">Back to inbox</button></form></p>';
+echo '<p><form action="messages.php" method="post"><input type="hidden" name="timeOffset" value="' . htmlspecialchars($_POST['timeOffset']) . '" /><input type="hidden" name="for" value="' . htmlspecialchars($_POST['from']) . '" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><button type="submit">Back to inbox</button></form></p>';
 $result = mysql_query("SELECT * FROM `msglist` WHERE `from` = '" . $_POST['from'] . "' ORDER BY `time` DESC");
 while ($row = mysql_fetch_assoc($result)) {
 	echo '<p class="topline">';
-	echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . $row['to'] . '" target="_blank">' . $row['to'] . '</a><br>Message:<br>' . htmlspecialchars_decode(stripslashes($row['msg']));
+	echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['to']) . '" target="_blank">' . htmlspecialchars($row['to']) . '</a><br>Message:<br>' . htmlspecialchars(stripslashes($row['msg']));
 	echo '</p>';
 }
 ?>
