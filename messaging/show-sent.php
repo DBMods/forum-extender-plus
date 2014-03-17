a82f8a7668a26cf95103c14bbc1670b0ffd829c6
Nathan Cheek <myself@nathancheek.com>
2014-03-17 15:38:58 -0400

    Fix newlines

--- messaging/show-sent.php
+++ messaging/show-sent.php
@@ -3,7 +3,7 @@ echo '<p><form action="messages.php" method="post"><input type="hidden" name="ti
 $result = mysql_query("SELECT * FROM `msglist` WHERE `from` = '" . $_POST['from'] . "' ORDER BY `time` DESC");
 while ($row = mysql_fetch_assoc($result)) {
 	echo '<p class="topline">';
-	echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['to']) . '" target="_blank">' . htmlspecialchars($row['to']) . '</a><br>Message:<br>' . htmlspecialchars(stripslashes($row['msg']));
+	echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['to']) . '" target="_blank">' . htmlspecialchars($row['to']) . '</a><br>Message:<br>' . nl2br(htmlspecialchars(stripslashes($row['msg'])));
 	echo '</p>';
 }
 ?>
