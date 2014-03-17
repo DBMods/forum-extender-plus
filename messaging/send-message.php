99731be32cd5bfa1c66b54314c08fd5293e05a4d
TechGeek01 <techgeek01help@gmail.com>
2014-03-17 18:04:00 -0500

    ?

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 $dest = $_POST['msgto'];
 if ($dest == "Andy Y.") {
   $dest = '1618104';
@@ -15,6 +16,11 @@ if ($dest == "Andy Y.") {
 }
 if (preg_replace('/[\D]/', '', $dest) == $dest && $dest != '') {
 	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($dest) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
+=======
+//$dest = $_POST['msgto'];
+if ($msgto_error !== 1) {
+	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($msgto) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
+>>>>>>> origin/master
 	echo '<p>Message sent.</p>';
 } else
 	echo '<p>Sorry, invalid destination.<br><form class="menu" action="messages.php" method="post"><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input type="hidden" name="timeOffset" value="' . $_POST['timeOffset'] . '" /><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><br><input name="from" type="hidden" value = "' . $_POST['for'] . '"/><input type="hidden" name="msgtext" value="' . $_POST['msgtext'] . '" /><br><button type="submit">Go Back</button></form></p>'
