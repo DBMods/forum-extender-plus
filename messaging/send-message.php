2868d77ccb173b49a7c33fa0df0dabf68f319f98
TechGeek01 <techgeek01help@gmail.com>
2014-03-17 18:23:14 -0500

    Minified code

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -1,18 +1,17 @@
 <?php
 $dest = $_POST['msgto'];
-if ($dest == "Andy Y.") {
+if ($dest == "Andy Y.")
 	$dest = '1618104';
-} elseif ($dest == "Chris J.") {
+elseif ($dest == "Chris J.")
 	$dest = '175532';
-} elseif ($dest == "Mark Mc") {
+elseif ($dest == "Mark Mc")
 	$dest = '30385';
-} elseif ($dest == "Nathan C.") {
+elseif ($dest == "Nathan C.")
 	$dest = '857279';
-} elseif ($dest == "R.M.") {
+elseif ($dest == "R.M.")
 	$dest = '643099';
-} elseif ($dest == "thightower" || $dest == "T. Hightower") {
+elseif ($dest == "thightower" || $dest == "T. Hightower")
 	$dest = '222573';
-}
 if (preg_replace('/[\D]/', '', $dest) == $dest && $dest != '') {
 	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($dest) . '", "' . mysql_real_escape_string($_POST['for']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
 	//$dest = $_POST['msgto'];
