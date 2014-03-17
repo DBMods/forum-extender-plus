0071138b6b13b7aea57a391a4fce8e2fde5400ff
Nathan Cheek <myself@nathancheek.com>
2014-03-17 17:50:38 -0400

    Add names for addressing and id check

--- messaging/process-message.php
+++ messaging/process-message.php
@@ -1,6 +1,24 @@
 <?php
 require 'db-login.php';
-$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($_POST['msgto']) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
-mysql_close($db);
+$msgto = $_POST['msgto'];
+if ($msgto == "Andy Y.") {
+  $msgto = 1618104;
+}elseif ($msgto == "Chris J.") {
+  $msgto = 175532;
+}elseif ($msgto == "Mark Mc") {
+  $msgto = 30385;
+}elseif ($msgto == "Nathan C.") {
+  $msgto = 857279;
+}elseif ($msgto == "R.M.") {
+  $msgto = 643099;
+}elseif ($msgto == "thightower" || $msgto == "T. Hightower") {
+  $msgto = 222573;
+}
+if (isnumeric($msgto)) {
+  $result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($msgto) . '", "' . mysql_real_escape_string($_POST['msgfrom']) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
+  mysql_close($db);
+} else {
+  $msgto_error = 1;
+}
 header('Location: ' . $_POST['returnto']);
 ?>
