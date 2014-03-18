ec40ef0def1f16a379b92ca32a5ebbc62b1496cb
TechGeek01 <techgeek01help@gmail.com>
2014-03-18 16:16:51 -0500

    If you aren't authenticated, you can no longer access the message page.

--- messaging/messages.php
+++ messaging/messages.php
@@ -33,30 +33,33 @@ if ($_POST['timeOffset'])
 			$userid = $_COOKIE['forumid'];
 			$timeoffset = $_COOKIE['timeoffset'];
 			require 'db-login.php';
-			echo '<p><a href="' . $_POST['returnto'] . '">Back to forums</a></p>';
-			$action = $_POST['action'];
-			$timeOffsetSeconds = $timeoffset * 60;
-			if ($action == 'delete')
-				include 'delete-message.php';
-			elseif ($action == 'addressbook')
-				include 'address-book.php';
-			elseif ($action == 'compose')
-				include 'compose-message.php';
-			elseif ($action == 'send')
-				include 'send-message.php';
-			elseif ($action == 'showsent')
-				include 'show-sent.php';
-			if ($userid && $action != 'addressbook' && $action != 'compose' && $action != 'showsent') {
-				echo '<p class="topline"><form action="messages.php" method="post" class="menu"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . htmlspecialchars($_POST['returnto']) . '" /><button type="submit">Compose</button></form><form action="messages.php" method="post" class="menu"><input type="hidden" name="action" value="showsent" /><input type="hidden" name="returnto" value="' . htmlspecialchars($_POST['returnto']) . '" /><input type="hidden" name="from" value="' . $userid . '" /><button type="submit">Show Sent Messages</button></form></p>';
-				$result = mysql_query("SELECT * FROM `msglist` WHERE `to` = '" . $userid . "' ORDER BY `time` DESC");
-				while ($row = mysql_fetch_assoc($result)) {
-					echo '<p class="topline">';
-					echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="action" value="delete" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="time" type="hidden" value="' . htmlspecialchars($row['time']) . '" /><input name="for" type="hidden" value="' . $userid . '" /><input type="hidden" name="from" value="' . htmlspecialchars($row['from']) . '" /><input type="hidden" name="msg" value="' . htmlspecialchars(stripslashes($row['msg'])) . '" /><button type="submit">Delete</button></form>';
-					echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value="' . htmlspecialchars(stripslashes($row['msg'])) . '"/><input name="to" type="hidden" value="' . htmlspecialchars($row['from']) . '" /><button type="submit">Reply</button></form>';
-					echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['from']) . '" target="_blank">' . htmlspecialchars($row['from']) . '</a><br>Message:<br>' . nl2br(htmlspecialchars(stripslashes($row['msg'])));
-					echo '</p>';
+			if ($userid) {
+				echo '<p><a href="' . $_POST['returnto'] . '">Back to forums</a></p>';
+				$action = $_POST['action'];
+				$timeOffsetSeconds = $timeoffset * 60;
+				if ($action == 'delete')
+					include 'delete-message.php';
+				elseif ($action == 'addressbook')
+					include 'address-book.php';
+				elseif ($action == 'compose')
+					include 'compose-message.php';
+				elseif ($action == 'send')
+					include 'send-message.php';
+				elseif ($action == 'showsent')
+					include 'show-sent.php';
+				if ($action != 'addressbook' && $action != 'compose' && $action != 'showsent') {
+					echo '<p class="topline"><form action="messages.php" method="post" class="menu"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . htmlspecialchars($_POST['returnto']) . '" /><button type="submit">Compose</button></form><form action="messages.php" method="post" class="menu"><input type="hidden" name="action" value="showsent" /><input type="hidden" name="returnto" value="' . htmlspecialchars($_POST['returnto']) . '" /><input type="hidden" name="from" value="' . $userid . '" /><button type="submit">Show Sent Messages</button></form></p>';
+					$result = mysql_query("SELECT * FROM `msglist` WHERE `to` = '" . $userid . "' ORDER BY `time` DESC");
+					while ($row = mysql_fetch_assoc($result)) {
+						echo '<p class="topline">';
+						echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="action" value="delete" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="time" type="hidden" value="' . htmlspecialchars($row['time']) . '" /><input name="for" type="hidden" value="' . $userid . '" /><input type="hidden" name="from" value="' . htmlspecialchars($row['from']) . '" /><input type="hidden" name="msg" value="' . htmlspecialchars(stripslashes($row['msg'])) . '" /><button type="submit">Delete</button></form>';
+						echo '<form method="post" action="messages.php" class="menu"><input type="hidden" name="action" value="compose" /><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="context" type="hidden" value="' . htmlspecialchars(stripslashes($row['msg'])) . '"/><input name="to" type="hidden" value="' . htmlspecialchars($row['from']) . '" /><button type="submit">Reply</button></form>';
+						echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['from']) . '" target="_blank">' . htmlspecialchars($row['from']) . '</a><br>Message:<br>' . nl2br(htmlspecialchars(stripslashes($row['msg'])));
+						echo '</p>';
+					}
 				}
-			}
+			} else
+				echo '<p>You do not have sufficient permission to access this page. Please authenticate through the <a href="https://forums.dropbox.com">Dropbox Forums</a>.</p>';
 			mysql_close($db);
 			?>
 		</div>
