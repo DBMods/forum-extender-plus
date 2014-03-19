a6ed745104a73d270c4fc091a97bca2dc4d21165
TechGeek01 <techgeek01help@gmail.com>
2014-03-19 07:03:30 -0500

    Holy cow, we probably should have noticed that one sooner.

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -16,5 +16,5 @@ if (is_numeric($dest) && $dest != 0) {
 	$result = mysql_query('INSERT INTO msglist VALUES("' . mysql_real_escape_string($dest) . '", "' . mysql_real_escape_string($userid) . '", "' . mysql_real_escape_string($_POST['msgtext']) . '", "' . time() . '")');
 	echo '<p>Message sent.</p>';
 } else
-	echo '<p>Sorry, invalid destination.<br><form class="menu" action="messages.php" method="post"><input type="hidden" name="returnto" value="' . $_POST['returnto'] . '" /><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><br><input type="hidden" name="msgtext" value="' . $_POST['msgtext'] . '" /><br><button type="submit">Go Back</button></form></p>';
+	echo '<p>Sorry, invalid destination.<br><form class="menu" action="messages.php" method="post"><input type="hidden" name="returnto" value="' . htmlspecialchars($_POST['returnto']) . '" /><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><br><input type="hidden" name="msgtext" value="' . $_POST['msgtext'] . '" /><br><button type="submit">Go Back</button></form></p>';
 ?>
