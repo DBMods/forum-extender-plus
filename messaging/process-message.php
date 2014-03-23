77632ab3c5df742f7d576c387ebf603cde400fba
TechGeek01 <techgeek01help@gmail.com>
2014-03-23 15:07:31 -0500

    We don't need names here

--- messaging/process-message.php
+++ messaging/process-message.php
@@ -1,20 +1,7 @@
 <?php
 require 'db-login.php';
-$msgto = $_POST['msgto'];
-if ($msgto == "Andy Y.")
-	$msgto = 1618104;
-elseif ($msgto == "Chris J.")
-	$msgto = 175532;
-elseif ($msgto == "Mark Mc")
-	$msgto = 30385;
-elseif ($msgto == "Nathan C.")
-	$msgto = 857279;
-elseif ($msgto == "R.M.")
-	$msgto = 643099;
-elseif ($msgto == "thightower" || $msgto == "T. Hightower")
-	$msgto = 222573;
-if (is_numeric($msgto)) {
-	$result = mysqli_query($db, 'INSERT INTO msglist VALUES("' . sqlesc($msgto) . '", "' . sqlesc($_POST['msgfrom']) . '", "' . sqlesc($_POST['msgtext']) . '", "' . time() . '")');
+if (is_numeric($_POST['msgto'])) {
+	$result = mysqli_query($db, 'INSERT INTO msglist VALUES("' . sqlesc($_POST['msgto']) . '", "' . sqlesc($_POST['msgfrom']) . '", "' . sqlesc($_POST['msgtext']) . '", "' . time() . '")');
 	mysqli_close($db);
 }
 header('Location: ' . $_POST['returnto']);
