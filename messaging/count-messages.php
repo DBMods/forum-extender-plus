6567418aba4ea6fb82d061ec3756292560471f1a
TechGeek01 <techgeek01help@gmail.com>
2014-03-22 21:45:10 -0500

    Rewrote counting mechanism

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -1,10 +1,6 @@
 <?php
 require 'db-login.php';
 $result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . $_GET['to'] . "'");
-$count = 0;
-while ($row = mysqli_fetch_assoc($result)) {
-	$count = $count + 1;
-}
-echo $count;
+echo mysqli_num_rows($result);
 mysqli_close($db);
 ?>
