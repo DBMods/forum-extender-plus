9592e479eeebffb7ba8efaa2d19973abd67bbc5f
TechGeek01 <techgeek01help@gmail.com>
2014-03-23 13:19:26 -0500

    Removed unnecessary <br>tags

--- messaging/send-message.php
+++ messaging/send-message.php
@@ -16,5 +16,5 @@ if (is_numeric($dest) && $dest != 0) {
 	$result = mysqli_query($db, 'INSERT INTO msglist VALUES("' . sqlesc($dest) . '", "' . sqlesc($userid) . '", "' . sqlesc($_POST['msgtext']) . '", "' . time() . '")');
 	echo '<div class="alert-center"><div id="alert-fade" class="alert alert-success"><p><strong>Message sent.</strong></p></div></div>';
 } else
-	echo '<div class="alert-center"><div class="alert alert-danger"><p><strong>Sorry, invalid destination.</strong><br><form action="messages.php" method="post"><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><br><input type="hidden" name="msgtext" value="' . htmlspecialchars($_POST['msgtext']) . '" /><br><button type="submit" class="btn btn-primary">Go Back</button></form></p></div></div>';
+	echo '<div class="alert-center"><div class="alert alert-danger"><p><strong>Sorry, invalid destination.</strong><form action="messages.php" method="post"><input name="action" type="hidden" value="compose" /><input name="to" type="hidden" value="' . $dest . '"/><input type="hidden" name="msgtext" value="' . htmlspecialchars($_POST['msgtext']) . '" /><button type="submit" class="btn btn-primary">Go Back</button></form></p></div></div>';
 ?>
