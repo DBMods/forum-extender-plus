9104286d557d940b22a55658532d90d37732b200
TechGeek01 <techgeek01help@gmail.com>
2014-03-26 16:30:23 -0500

    Removed unnecessary form data

--- messaging/compose-message.php
+++ messaging/compose-message.php
@@ -25,11 +25,11 @@ if ($action == 'compose' || $senderror) {
 	echo $senderror;
 	echo '<h2>Compose</h2>';
 	echo '<div class="topline">';
-	echo '<form action="messages.php" method="post"><input name="action" type="hidden" value="addressbook" /><br><button type="submit" class="btn btn-primary">View Address Book</button></form>';
+	echo '<br><form action="" method="post"><button type="submit" class="btn btn-primary" name="action" value="addressbook">View Address Book</button></form>';
 	if ($_POST['context'])
 		echo '<p>' . htmlspecialchars($_POST['context']) . '</p>';
-	echo '<form id="messageform" action="messages.php" method="post"><input name="action" type="hidden" value="send"/><input id="msgto" name="msgto" type="textbox" style="width:100%" class="form-control" placeholder="Recipient*" value="' . htmlspecialchars($_POST['msgto']) . '" required=""/><br><textarea name="msgtext" placeholder="Message*" rows="9" style="width:100%" class="form-control" required="">' . htmlspecialchars($_POST['msgtext']) . '</textarea><br><div style="height:30px;margin-top:-20px;"><button type="submit" class="btn btn-success btn-left">Send</button></form>';
-	echo '<form action="messages.php" method="post"><button type="submit" class="btn btn-danger btn-right" style="margin-right:0px">Cancel</button></form></div>';
+	echo '<form id="messageform" action="" method="post"><input id="msgto" name="msgto" type="textbox" style="width:100%" class="form-control" placeholder="Recipient*" value="' . htmlspecialchars($_POST['msgto']) . '" required=""/><br><textarea name="msgtext" placeholder="Message*" rows="9" style="width:100%" class="form-control" required="">' . htmlspecialchars($_POST['msgtext']) . '</textarea><br><div style="height:30px;margin-top:-20px;"><button type="submit" class="btn btn-success btn-left" name="action" value="send">Send</button></form>';
+	echo '<form action="" method="post"><button type="submit" class="btn btn-danger btn-right" style="margin-right:0px">Cancel</button></form></div>';
 	echo '</div>';
 }
 ?>
--- messaging/show-sent.php
+++ messaging/show-sent.php
@@ -4,8 +4,6 @@ echo '<h2>Sent Messages</h2>';
 navform();
 $result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `from` = '" . $userid . "' ORDER BY `time` DESC");
 while ($row = mysqli_fetch_assoc($result)) {
-	echo '<p class="topline">';
-	echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['to']) . '" target="_blank">' . htmlspecialchars($row['to']) . '</a><br>Message:<br>' . nl2br(htmlspecialchars($row['msg']));
-	echo '</p>';
+	echo '<p class="topline"><br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: <a href="https://forums.dropbox.com/profile.php?id=' . htmlspecialchars($row['to']) . '" target="_blank">' . htmlspecialchars($row['to']) . '</a><br>Message:<br>' . nl2br(htmlspecialchars($row['msg'])) . '</p>';
 }
 ?>
