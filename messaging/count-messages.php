c4643f9567a3d6a65046abcc93ba25e481f3b55f
TechGeek01 <techgeek01help@gmail.com>
2014-03-26 14:07:28 -0500

    Don't include archived messages in count

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -1,6 +1,6 @@
 <?php
 require 'db-login.php';
-$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . $_GET['to'] . "'");
+$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . $_GET['to'] . "' AND `archived` = 0");
 echo mysqli_num_rows($result);
 mysqli_close($db);
 ?>
