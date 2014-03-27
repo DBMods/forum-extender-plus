b1ad9475ab2058dffb0306be9680eddadcf8dd54
TechGeek01 <techgeek01help@gmail.com>
2014-03-26 20:45:07 -0500

    Stats spacing fix

--- messaging/stats.php
+++ messaging/stats.php
@@ -3,7 +3,7 @@ $showinbox = false;
 echo '<h2>Stats</h2>';
 $result = mysqli_query($db, "SELECT * FROM msglist WHERE id = (SELECT MAX(id) FROM msglist)");
 $row = mysqli_fetch_assoc($result);
-echo '<div class="topline center">';
-echo '<p>Messages sent: ' . $row['id'] . '</p>';
-echo '</div>';
+echo '<p class="topline center"><br>';
+echo 'Messages sent: ' . $row['id'];
+echo '</p>';
 ?>
