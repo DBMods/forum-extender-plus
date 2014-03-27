875d8e746d5775cec6592ad33eada18663d3ae38
TechGeek01 <techgeek01help@gmail.com>
2014-03-26 20:46:00 -0500

    Aesthetic fix

--- messaging/stats.php
+++ messaging/stats.php
@@ -3,7 +3,7 @@ $showinbox = false;
 echo '<h2>Stats</h2>';
 $result = mysqli_query($db, "SELECT * FROM msglist WHERE id = (SELECT MAX(id) FROM msglist)");
 $row = mysqli_fetch_assoc($result);
-echo '<p class="topline center"><br>';
+echo '<p class="topline"><br>';
 echo 'Messages sent: ' . $row['id'];
 echo '</p>';
 ?>
