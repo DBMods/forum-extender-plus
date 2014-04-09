5f9337ed5509ab605494e72440b6d6abda6ebd37
Andy Yasger <techgeek01help@gmail.com>
2014-04-09 18:02:48 -0500

    New placeholder text for message field

--- messaging/compose-message.php
+++ messaging/compose-message.php
@@ -22,13 +22,14 @@ if ($action == 'send') {
 }
 if ($action == 'compose' || $senderror) {
 	$showinbox = false;
+	$msgplaceholder = 'This is where you would write your message. You know, sort of like this. I mean, you probably wouldn\'t actually write this up to send to someone, but if you were, that would be a proper use for the message system. I mean, you don\'t have to write a novel or anything, but you don\'t have to not write a novel. Heck, feel free to pour your heart and soul out into your message. Actually, wait, don\'t. That might be kinda weird. So anyway, like I said, it doesn\'t have to be a novel, but it can be. Novels are fun! It\'s almost like you\'re writing a book, but it has a fancy name. So, I guess, write what you want here, and have fun with it!';
 	echo $senderror;
 	echo '<h2>Compose</h2>';
 	echo '<div class="topline">';
 	echo '<br><form action="" method="post"><button type="submit" class="btn btn-primary" name="action" value="addressbook">View Address Book</button></form>';
 	if ($_POST['context'])
 		echo '<p>' . nl2br(htmlspecialchars($_POST['context'])) . '</p>';
-	echo '<form id="messageform" action="" method="post"><input id="msgto" name="msgto" type="textbox" style="width:100%" class="form-control" placeholder="Recipient*" value="' . htmlspecialchars($_POST['msgto']) . '" required=""/><br><textarea name="msgtext" placeholder="Message*" rows="9" style="width:100%" class="form-control" required="">' . htmlspecialchars($_POST['msgtext']) . '</textarea><br><div style="height:30px;margin-top:-20px;"><button type="submit" class="btn btn-success btn-left" name="action" value="send">Send</button></form>';
+	echo '<form id="messageform" action="" method="post"><input id="msgto" name="msgto" type="textbox" style="width:100%" class="form-control" placeholder="Recipient*" value="' . htmlspecialchars($_POST['msgto']) . '" required=""/><br><textarea name="msgtext" placeholder="' . $msgplaceholder . '" rows="9" style="width:100%" class="form-control" required="">' . htmlspecialchars($_POST['msgtext']) . '</textarea><br><div style="height:30px;margin-top:-20px;"><button type="submit" class="btn btn-success btn-left" name="action" value="send">Send</button></form>';
 	echo '<form action="" method="post"><button type="submit" class="btn btn-danger btn-right" style="margin-right:0px">Cancel</button></form></div>';
 	echo '</div>';
 }
