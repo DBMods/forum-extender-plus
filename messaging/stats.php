a9afec06b4829a98bdb1e34d9dd02d1e1276bb7b
Nathan Cheek <myself@nathancheek.com>
2014-04-10 18:11:47 -0400

    styled stats page

--- messaging/stats.php
+++ messaging/stats.php
@@ -3,7 +3,8 @@ $showinbox = false;
 echo '<h2>Stats</h2>';
 $result = mysqli_query($db, 'SHOW TABLE STATUS LIKE "msglist"');
 $row = mysqli_fetch_assoc($result);
+$msgcount = $row['Auto_increment'] - 1;
 echo '<p class="topline"><br>';
-echo '<div class="small-center"><div class="panel panel-primary"><div class="panel-heading"><h3>Messages sent</h3></div><div class="panel-body"><h2>' . ($row['Auto_increment'] - 1) . '</h2></div></div></div>';
+echo '<div class="small-center"><div class="panel panel-primary"><div class="panel-heading"><h3>Messages sent</h3></div><div class="panel-body"><h2><input id="msg-count" /></h2></div></div></div>';
 echo '</p>';
 ?>
