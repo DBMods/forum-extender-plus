e980987a7cca5de66fe9a62528e3678a29ab1208
Nathan Cheek <myself@nathancheek.com>
2014-04-10 10:05:49 -0400

    Change stats design

--- messaging/stats.php
+++ messaging/stats.php
@@ -4,6 +4,6 @@ echo '<h2>Stats</h2>';
 $result = mysqli_query($db, 'SHOW TABLE STATUS LIKE "msglist"');
 $row = mysqli_fetch_assoc($result);
 echo '<p class="topline"><br>';
-echo 'Messages sent: ' . ($row['Auto_increment'] - 1);
+echo '<div class="small-center"><div class="panel panel-primary"><div class="panel-heading"><h3>Messages sent</h3></div><div class="panel-body"><h2>' . ($row['Auto_increment'] - 1) . '</h2></div></div></div>';
 echo '</p>';
 ?>
