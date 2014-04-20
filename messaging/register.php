89ccf92b3eba98b93b9318b192313b296c5061ea
Andy Yasger <techgeek01help@gmail.com>
2014-04-20 13:55:14 -0500

    Fixed formatting so that Aptana Studio doesn't have a fit
    In Aptana, it doesn't like it if you open curly brackets in PHP, and then stick HTML in there and another block of PHP with a closing curly bracket.
    
    It also doesn't like
    
    $var = mysqli_fetch_row($result)[X];
    
    It must be
    
    $var = mysqli_fetch_row($result);
    $var = $var[X];
    
    Otherwise it calls a syntax error.

--- messaging/register.php
+++ messaging/register.php
@@ -1,101 +1,85 @@
 <?php
-function registerPanel($userid){
-?>
-<div class="small-center">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h3>Register</h3>
-		</div>
-		<div class="panel-body">
-			<form method="post" action="">
-				<fieldset>
-					<div class="form-group">
-						<input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" />
-					</div>
-					<div class="form-group">
-						<input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" />
-					</div>
-					<div class="form-group">
-						<input name="userid" type="hidden" value="<?php echo $userid?>" /><button name="action" class="btn btn-success" value="create-account">Create account</button>
-					</div>
-				</fieldset>
-			</form>
-		</div>
-	</div>
-</div>
-<?php
+function registerPanel($userid) {
+	echo '<div class="small-center">';
+	echo '<div class="panel panel-primary">';
+	echo '<div class="panel-heading"><h3>Register</h3></div>';
+	echo '<div class="panel-body">';
+	echo '<form method="post" action="">';
+	echo '<fieldset>';
+	echo '<div class="form-group"><input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group"><input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group"><input name="userid" type="hidden" value="' . $userid . '" /><button name="action" class="btn btn-success" value="create-account">Create account</button></div>';
+	echo '</fieldset>';
+	echo '</form>';
+	echo '</div>';
+	echo '</div>';
+	echo '</div>';
 }
-if($_POST['action']=="create-account" && is_numeric($_POST['userid'])){//Request to create an account from extension
-	$userid=$_POST['userid'];
-	$result = mysqli_query($db, "SELECT * FROM `users` WHERE `userid` = '" . sqlesc($_POST['userid']) ."'");//checks for account already existing with userid
+if ($_POST['action'] == "create-account" && is_numeric($_POST['userid'])) { //Request to create an account from extension
+	$userid = $_POST['userid'];
+	$result = mysqli_query($db, "SELECT * FROM `users` WHERE `userid` = '" . sqlesc($_POST['userid']) . "'");
+	//checks for account already existing with userid
 	$account_exist = mysqli_fetch_row($result);
-	if(!$account_exist && !$_POST['username']){//If account does not already exist and user did not fill out register form
+	if (!$account_exist && !$_POST['username']) //If account does not already exist and user did not fill out register form
 		registerPanel($userid);
-	}elseif(!$account_exist && $_POST['username']){//If account does not already exist and user did fill out register form
-		$result = mysqli_query($db, "SELECT * FROM `users` WHERE username = '" . sqlesc($_POST['username']) . "'");//Check if username is already in use
-		$row=mysqli_fetch_array($result);
-		if ($row) {//Username is already in use
+	elseif (!$account_exist && $_POST['username']) { //If account does not already exist and user did fill out register form
+		$result = mysqli_query($db, "SELECT * FROM `users` WHERE username = '" . sqlesc($_POST['username']) . "'");
+		//Check if username is already in use
+		$row = mysqli_fetch_array($result);
+		if ($row) { //Username is already in use
 			$usernameUsed = true;
 			echo "<div class='alert-center'><div id='alert-fade' class='alert alert-warning'><p><strong>Username already in use!</strong></p></div></div>";
 			registerPanel($userid);
-		}else{//Username is not already in use
-			$chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-			for($i=0;$i<10;$i++) {
-				$random .=$chars[rand(0,strlen($chars)-1)];
+		} else { //Username is not already in use
+			$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+			for ($i = 0; $i < 10; $i++) {
+				$random .= $chars[rand(0, strlen($chars) - 1)];
 			}
 			//check if token already exists and if so gen another one
-			$exists=true;
-			while($exists==true) {
-				$query="SELECT * FROM `users` WHERE `ext_token` = '$random'";
-				$result=mysqli_query($sqlconnect,$query);
-				$row=mysqli_fetch_array($result);
-				if($row !== NULL) {
-					$random="";
-					for($i=0;$i<10;$i++) {
-						$random .=$chars[rand(0,strlen($chars)-1)];
+			$exists = true;
+			while ($exists == true) {
+				$query = "SELECT * FROM `users` WHERE `ext_token` = '$random'";
+				$result = mysqli_query($sqlconnect, $query);
+				$row = mysqli_fetch_array($result);
+				if ($row !== NULL) {
+					$random = "";
+					for ($i = 0; $i < 10; $i++) {
+						$random .= $chars[rand(0, strlen($chars) - 1)];
 					}
-				} else {
-					$exists=false;
-				}
+				} else
+					$exists = false;
 			}
 			$token = $random;
 			$username = htmlspecialchars($_POST['username']);
 			$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 			$create_time = time();
 			$create_ip = $_SERVER['REMOTE_ADDR'];
-			$result = mysqli_query($db, "INSERT INTO `users` (userid, username, password, ext_token, create_time, create_ip) VALUES ('" . sqlesc($userid) ."', '" . sqlesc($username) . "', '" . sqlesc($password) . "', '" . sqlesc($token) . "', '" . sqlesc($create_time) . "', '" . sqlesc($create_ip) . "')");
+			$result = mysqli_query($db, "INSERT INTO `users` (userid, username, password, ext_token, create_time, create_ip) VALUES ('" . sqlesc($userid) . "', '" . sqlesc($username) . "', '" . sqlesc($password) . "', '" . sqlesc($token) . "', '" . sqlesc($create_time) . "', '" . sqlesc($create_ip) . "')");
 			echo '<h4 class="center">Account created. Click <a href="https://forums.dropbox.com/?msgtoken=' . $token . '">here</a> to finish the account creation process.</h4><p class="center">In order to finish the account creation process, we must redirect you back to the forums. However, this will only happen during registration.</p>';
 		}
 	}
-	if($account_exist){//account already exists login form + return token to extension
-		signinPanel("showTokenRedir","pass-token");
-	}
-}elseif($_POST['action']=="pass-token" && $_POST['username'] && $_POST['password']){
+	if ($account_exist) //account already exists login form + return token to extension
+		signinPanel("showTokenRedir", "pass-token");
+} elseif ($_POST['action'] == "pass-token" && $_POST['username'] && $_POST['password']) {
 	$result = mysqli_query($db, "SELECT password FROM `users` WHERE username = '" . sqlesc($_POST['username']) . "'");
-	$passwordHash = mysqli_fetch_row($result)['0'];
-	if(password_verify($_POST['password'], $passwordHash)) {
-		$userAuthenticated=true;
-	}
-	if($userAuthenticated) {//authentication successful
+	$passwordHash = mysqli_fetch_row($result);
+	$passwordHash = $passwordHash['0'];
+	if (password_verify($_POST['password'], $passwordHash))
+		$userAuthenticated = true;
+	if ($userAuthenticated) { //authentication successful
 		$result = mysqli_query($db, "SELECT ext_token FROM `users` WHERE username = '" . sqlesc($_POST['username']) . "'");
-		$token = mysqli_fetch_row($result)['0'];
+		$token = mysqli_fetch_row($result);
+		$token = $token['0'];
 		echo '<h4 class="center">Successfully signed in. Click <a href="https://forums.dropbox.com/?msgtoken=' . $token . '">here</a> to finish the sign in process.</h4><p class="center">In order to finish the sign in process, we must redirect you back to the forums. However, this will only happen once.</p>';
-	}else{//authentication unsuccessful
+	} else { //authentication unsuccessful
 		badAuth();
-		signinPanel("showTokenRedir","pass-token");
+		signinPanel("showTokenRedir", "pass-token");
 	}
-}else{//No request from extension to create the account
-?>
-<div class="small-center">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h3>Sign up</h3>
-		</div>
-		<div class="panel-body">
-			<p>The Dropbox Forum Extender+ extension is required to use this site.  Please install from <a href="https://github.com/dbmods/forum-extender-plus">Github</a>
-		</div>
-	</div>
-</div>
-<?php
+} else { //No request from extension to create the account
+	echo '<div class="small-center">';
+	echo '<div class="panel panel-primary">';
+	echo '<div class="panel-heading"><h3>Sign up</h3></div>';
+	echo '<div class="panel-body"><p>The Dropbox Forum Extender+ extension is required to use this site.  Please install from <a href="https://github.com/dbmods/forum-extender-plus">Github</a></div>';
+	echo '</div></div>';
 }
 ?>
--- messaging/sign-in.php
+++ messaging/sign-in.php
@@ -1,41 +1,37 @@
 <?php
-function signinPanel($showOption,$addAction){
-?>
-<div class="small-center">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h3>Sign in</h3>
-		</div>
-		<div class="panel-body">
-			<form method="post" action="">
-				<fieldset>
-					<div class="form-group">
-						<input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" />
-					</div>
-					<div class="form-group">
-						<input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" />
-					</div>
-					<div class="form-group">
-						<?php if($addAction){echo "<button name=\"action\" value=\"" . $addAction . "\" class=\"btn btn-success\">Sign in</button>";}else{echo "<button class=\"btn btn-success\">Sign in</button>";}?>
-					</div>
-				</fieldset>
-			</form>
-			<?php if($showOption=="showRegister"){echo "<p>Not registered? <form method='post' action=''><button name='action' class='btn btn-success' value='register'>Sign up!</button></form></p>";}?>
-			<?php if($showOption=="showTokenRedir"){echo "<p>Sign in to allow the extension to access your messaging account</p>";}?>
-		</div>
-	</div>
-</div>
-<?php
+function signinPanel($showOption, $addAction) {
+	echo '<div class="small-center">';
+	echo '<div class="panel panel-primary">';
+	echo '<div class="panel-heading"><h3>Sign in</h3></div>';
+	echo '<div class="panel-body">';
+	echo '<form method="post" action="">';
+	echo '<fieldset>';
+	echo '<div class="form-group"><input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group"><input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group">';
+	if ($addAction)
+		echo "<button name=\"action\" value=\"" . $addAction . "\" class=\"btn btn-success\">Sign in</button>";
+	else
+		echo "<button class=\"btn btn-success\">Sign in</button>";
+	echo '</div>';
+	echo '</fieldset>';
+	echo '</form>';
+	if ($showOption == "showRegister")
+		echo "<p>Not registered? <form method='post' action=''><button name='action' class='btn btn-success' value='register'>Sign up!</button></form></p>";
+	if ($showOption == "showTokenRedir")
+		echo "<p>Sign in to allow the extension to access your messaging account</p>";
+	echo '</div>';
+	echo '</div>';
+	echo '</div>';
 }
-function badAuth(){
+function badAuth() {
 	echo "<div class='alert-center'><div id='alert-fade' class='alert alert-danger'><p><strong>Wrong username or password</strong></p></div></div>";
 }
-if($action=="register" || $action=="create-account" || $action=="pass-token") {//clicked register button
+if ($action == "register" || $action == "create-account" || $action == "pass-token")//clicked register button
 	include "register.php";
-}
-if(!$userAuthenticated && $action!="register" && $action!="create-account" && $action!="pass-token"){//If authentication unsuccessful or not tried
-	if($badAuth) {
+if (!$userAuthenticated && $action != "register" && $action != "create-account" && $action != "pass-token") {//If authentication unsuccessful or not tried
+	if ($badAuth)
 		badAuth();
-	}
 	signinPanel("showRegister");
 }
+?>
