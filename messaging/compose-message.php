4b9000350a3e894ee0613f5e950d4144a46b2c1c
Andy Yasger <techgeek01help@gmail.com>
2014-04-23 16:39:00 -0500

    Finished username-based sending

--- messaging/compose-message.php
+++ messaging/compose-message.php
@@ -14,6 +14,11 @@ if ($action == 'send') {
 		$dest = '643099';
 	elseif ($dest == 'thightower' || $dest == 'T. Hightower' || $dest == 'T Hightower')
 		$dest = '222573';
+	if (!is_numeric($dest)) {
+		$result = mysqli_query($db, 'SELECT * FROM `users` WHERE `username` = "' . $dest . '"');
+		$row = mysqli_fetch_assoc($result);
+		$dest = $row['userid'];
+	}
 	if (is_numeric($dest) && $dest != 0) {
 		mysqli_query($db, 'INSERT INTO msglist (`to`, `from`, `msg`, `time`) VALUES("' . sqlesc($dest) . '", "' . sqlesc($userid) . '", "' . sqlesc($_POST['msgtext']) . '", "' . time() . '")');
 		echo '<div class="alert-center"><div id="alert-fade" class="alert alert-success"><p><strong>Message sent.</strong></p></div></div>';
