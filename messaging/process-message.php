467d77afc5bee6f32fdff194a5b37fd361b92e57
Andy Yasger <techgeek01help@gmail.com>
2014-04-28 18:56:06 -0500

    600 commits! Whoo!!!!!
    Included functions.php to eliminate separate declaration of sqlesc()

--- messaging/process-message.php
+++ messaging/process-message.php
@@ -1,9 +1,7 @@
 <?php
 require 'db-login.php';
-function sqlesc($string) {
-	global $db;
-	return mysqli_real_escape_string($db, $string);
-}
+require 'functions.php';
+
 $dest = $_POST['msgto'];
 if (is_numeric($dest) && $dest != 0) {
 	mysqli_query($db, 'INSERT INTO msglist (`to`, `from`, `msg`, `time`) VALUES("' . sqlesc($dest) . '", "' . sqlesc($_POST['msgfrom']) . '", "' . sqlesc($_POST['msgtext']) . '", "' . time() . '")');
