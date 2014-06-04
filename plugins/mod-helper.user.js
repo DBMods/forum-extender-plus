3cc7d7ed84c49aea7c835a604f08f1af834bbe4e
Andy Yasger <techgeek01help@gmail.com>
2014-06-03 19:02:58 -0500

    Added open/close thread links to forum pages

--- plugins/mod-helper.user.js
+++ plugins/mod-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace DropboxMods
 // @description Super User hel-per script for Forum Extender+
 // @include https://forums.dropbox.com/*
-// @version 1.0.0
+// @version 1.0.1
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @downloadURL https://github.com/DBMods/forum-extender-plus/raw/master/plugins/mod-helper.user.js
 // @updateURL https://github.com/DBMods/forum-extender-plus/raw/master/plugins/mod-helper.user.js
@@ -11,15 +11,25 @@
 // ==/UserScript==
 
 //Handle thread openning and closing from front page
+$('head').append('<style>.threadToggleLink:hover{cursor:pointer}</style>');
 $('#latest tr').find('td:first').each(function(index){
-    var topicId = $(this).find('a:first').attr('href').split('?id=')[1];
-    var thisStuff = $(this);
-    GM_xmlhttpRequest({
-        method: 'GET',
-        url: 'https://forums.dropbox.com/topic.php?id=' + topicId,
-        onload: function(response) {
-            var resp = response.responseText;
-            $(thisStuff).prepend('[<a href="https://forums.dropbox.com/bb-admin/topic-toggle.php?id=' + topicId + '&_wp_http_referrer=%2F&_wpnonce=' + resp.split('<div class="admin">')[1].split('<a href="https://forums.dropbox.com/bb-admin/topic-toggle.php')[1].split('">')[0].split('_wpnonce=')[1] + '">Open/Close</a>] - ');
-        }
-    });
+	$(this).prepend('<span class="forumext-mod-topic-toggle">[<span class="threadToggleLink">Toggle</span>]</span> - <menu type="context" id="forumext-mod-post-menu"><menuitem label="Open/Close Thread" onclick="window.location.href = link;"></menuitem></menu>');
 });
+$('.forumext-mod-topic-toggle').click(function(evt){
+	var thisStuff = $(this);
+	var topicId = $(this).parent().find('a:first').attr('href').split('?id=')[1];
+	GM_xmlhttpRequest({
+		method: 'GET',
+		url: 'https://forums.dropbox.com/topic.php?id=' + topicId,
+		onload: function(response) {
+			var resp = response.responseText;
+			var link = 'https://forums.dropbox.com/bb-admin/topic-toggle.php?id=' + topicId + '&_wpnonce=' + resp.split('<div class="admin">')[1].split('<a href="https://forums.dropbox.com/bb-admin/topic-toggle.php')[1].split('">')[0].split('_wpnonce=')[1];
+			$(thisStuff).html('[<a class="threadToggleLink" style="color:#000" href="' + link + '">Toggle</a>]');
+			setTimeout(function() {
+				$(thisStuff).find('.threadToggleLink')[0].click();
+			}, 1);
+		}
+	});
+});
+if (window.location.href.contains('message='))
+	window.location.href = window.location.href.replace('?message=opened', '').replace('?message=closed', '').replace('&message=opened', '').replace('&message=closed', '');
