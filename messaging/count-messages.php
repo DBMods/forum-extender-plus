b4cd8d907e6afda297f9cc45f46697e63fcf5ad9
Andy Yasger <techgeek01help@gmail.com>
2014-06-12 11:45:08 -0500

    Instructions for Nathan while I'm away

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -3,4 +3,8 @@ require 'db-login.php';
 $result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . $_GET['to'] . "' AND `archived` = 0");
 echo mysqli_num_rows($result);
 mysqli_close($db);
+
+//Make this require a token, which will be passed in as $_GET['token'], and if it's wrong, instead of appending a message count to the page like it does now with mysqli_num_rows($result),
+//append "Incorrect token" (without quotes, of course), and only that. Make sure it's exactly that if the token's wrong. Nothing more, nothing less. Then, the userscript will be able to respond, and head over to
+//gettoken.php to retrieve the token from the database.
 ?>
