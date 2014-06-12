cda0b84b4703dd23ef30d11a2f36a7d78b9033bb
Nathan Cheek <myself@nathancheek.com>
2014-06-12 19:42:35 -0400

    Allow non-authenticated users to report issues

--- messaging/report.php
+++ messaging/report.php
@@ -1,6 +1,6 @@
 <?php
 require 'header.php';
-if ($userAuthenticated) {
+//if ($userAuthenticated) {
 	echo '<h2>Report a Problem</h2>';
 	echo '<p class="topline">';
 	if ($action == 'report') {
@@ -15,9 +15,9 @@ if ($userAuthenticated) {
 	} elseif ($action == 'confirmreport') {
 		if ($_POST['comments'])
 			echo '<p>' . nl2br(htmlspecialchars($_POST['comments'])) . '</p>';
-		echo '<p>Are you sure you want to submit this report?<br><br><form action="./" method="post"><input type="hidden" name="msgto" value="Andy Y" /><input type="hidden" name="msgtext" value="' . $_POST['report'] . '&#10;&#10;Comment:&#10;' . $_POST['comments'] . '" /><button name="action" value="send" class="btn btn-success">Yes, report this</button><button name="action" value="report" class="btn btn-danger">No, start over</button></form></p>';
+		echo '<p>Are you sure you want to submit this report?<br><br><form action="./" method="post"><input type="hidden" name="msgto" value="TechGeek01" /><input type="hidden" name="msgtext" value="' . $_POST['report'] . '&#10;&#10;Comment:&#10;' . $_POST['comments'] . '" /><button name="action" value="send" class="btn btn-success">Yes, report this</button><button name="action" value="report" class="btn btn-danger">No, start over</button></form></p>';
 	}
 	echo '</p>';
-}
+//}
 require 'footer.php';
 ?>
