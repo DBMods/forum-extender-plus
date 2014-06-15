52560f5c893f6f644407bda556d874b2b966e0bd
Andy Yasger <techgeek01help@gmail.com>
2014-06-15 16:31:36 -0500

    Security and such

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -2,12 +2,12 @@
 require 'db-login.php';
 require 'functions.php';
 
-$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . $_GET['to'] . "' AND `archived` = 0");
-$msgcount = mysqli_num_rows($result);
 //If userToken and userid is posted, check login
 if ($_GET['to'] && $_GET['token']) {
 	$userToken = htmlspecialchars($_GET['token']);
 	$userid = htmlspecialchars($_GET['to']);
+	$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($userid) . "' AND `archived` = 0");
+	$msgcount = mysqli_num_rows($result);
 	$result = mysqli_query($db, "SELECT * FROM `users` WHERE (ext_token = '" . sqlesc($userToken) . "' AND userid = '" . sqlesc($userid) . "') LIMIT 1");
 	$row = mysqli_fetch_array($result);
 
