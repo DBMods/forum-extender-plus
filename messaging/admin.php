ecb0c99e2fa8774cbbf5c620bcf0f764c4963917
Andy Yasger <techgeek01help@gmail.com>
2015-01-23 13:58:24 -0600

    Updated user IDs

--- messaging/admin.php
+++ messaging/admin.php
@@ -2,7 +2,7 @@
 require 'header.php';
 if ($userAuthenticated) {
 	getMessages();
-  if ($userid=='857279' || $userid=='434617605') {
+  if ($userid=='433256745' || $userid=='434617605') {
     if ($_POST['dashmodapply']) {
       //If password is being changed
       $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
