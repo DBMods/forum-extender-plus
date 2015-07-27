6d562f54a481600a5fed2c5af7a9398561278b20
TechGeek01 <techgeek01help@gmail.com>
2015-07-27 18:14:09 -0500

    Formatting

--- messaging/sign-in.php
+++ messaging/sign-in.php
@@ -25,9 +25,9 @@ function signinPanel($showOption, $addAction) {
 function badAuth() {
 	echo "<div class='alert-center'><div id='alert-fade' class='alert alert-danger'><p><strong>Wrong username or password</strong></p></div></div>";
 }
-if ($action == "register" || $action == "create-account" || $action == "pass-token")//clicked register button
+if ($action == "register" || $action == "create-account" || $action == "pass-token") //clicked register button
 	include "register.php";
-if (!$userAuthenticated && $action != "register" && $action != "create-account" && $action != "pass-token") {//If authentication unsuccessful or not tried
+if (!$userAuthenticated && $action != "register" && $action != "create-account" && $action != "pass-token") { //If authentication unsuccessful or not tried
 	if ($badCookie) {
 		//When user changes password (and therefore token) the token cookie will be outdated
 		//TODO if token cookie is replaced with a session cookie, then we won't need this
