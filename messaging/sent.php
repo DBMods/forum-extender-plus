06d417b7813d2fe7702fe8d277135278f85ba4e5
TechGeek01 <techgeek01help@gmail.com>
2015-07-27 20:22:40 -0500

    Update message list view for sent messages page

--- messaging/sent.php
+++ messaging/sent.php
@@ -2,12 +2,12 @@
 require 'header.php';
 if ($userAuthenticated) {
 	getMessages();
-	
+
 	if ($showinbox) {
 		echo '<h2>Sent Messages</h2>';
-		$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `from` = '" . $userid . "' ORDER BY `time` DESC");
+		$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `from` = '" . $username . "' ORDER BY `time` DESC");
 		while ($row = mysqli_fetch_assoc($result)) {
-			echo '<p class="topline"><br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: ' . idToName(htmlspecialchars($row['to'])) . '<br>Message:<br>' . nl2br(htmlspecialchars($row['msg'])) . '</p>';
+			echo '<p class="topline"><br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>To: ' . htmlspecialchars($row['to']) . '<br>Message:<br>' . nl2br(htmlspecialchars($row['msg'])) . '</p>';
 		}
 	}
 }
