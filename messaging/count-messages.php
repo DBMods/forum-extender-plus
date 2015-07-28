ffb0d3ff0d329dcc654a6dfb538878e70f0ec0cf
TechGeek01 <techgeek01help@gmail.com>
2015-07-28 13:08:45 -0500

    Updated message counter to allow for a UID sanity check

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -10,11 +10,22 @@ if ($_GET['to'] && $_GET['token']) {
 	$row = mysqli_fetch_array($result);
 
 	if ($row) {
-		$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($row['username']) . "' AND `archived` = 0");
-		$msgcount = mysqli_num_rows($result);
-		echo $msgcount;
+		//We have authed properly
+		$cfg = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `config` WHERE setting = 'default_uid_origin' LIMIT 1"));
+
+		//Check to make sure our UID origin is from the right place
+		if ($row['uid_origin'] == $cfg['val']) {
+			//If origin is correct, return message count
+			$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($row['username']) . "' AND `archived` = 0");
+			$msgcount = mysqli_num_rows($result);
+			echo $msgcount;
+		} else {
+			//UID origin is wrong, so return error
+			echo 'Bad UID origin';
+		}
 	} else {
-		echo "Incorrect token";
+		//Bad auth, so token must be bad
+		echo 'Incorrect token';
 	}
 }
 mysqli_close($db);
