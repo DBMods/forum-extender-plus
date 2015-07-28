5ed8bae10c54d5b9adfd5a6608ec2c084c94e67f
TechGeek01 <techgeek01help@gmail.com>
2015-07-28 16:42:52 -0500

    Rewrite message counter to properly distinguish between bad UIDs and tokens

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -6,15 +6,17 @@ require 'functions.php';
 if ($_GET['to'] && $_GET['token']) {
 	$userToken = htmlspecialchars($_GET['token']);
 	$userid = htmlspecialchars($_GET['to']);
-	$result = mysqli_query($db, "SELECT * FROM `users` WHERE (ext_token = '" . sqlesc($userToken) . "' AND userid = '" . sqlesc($userid) . "') LIMIT 1");
+	$result = mysqli_query($db, "SELECT * FROM `users` WHERE ext_token = '" . sqlesc($userToken) . "' LIMIT 1");
 	$row = mysqli_fetch_array($result);
 
 	if ($row) {
-		//We have authed properly
+		//Our token matches, so assume bad UID
+		$result = mysqli_query($db, "SELECT * FROM `users` WHERE (ext_token = '" . sqlesc($userToken) . "' AND userid = '" . sqlesc($userid) . "') LIMIT 1");
+		$row = mysqli_fetch_array($result);
 		$cfg = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `config` WHERE setting = 'default_uid_origin' LIMIT 1"));
 
 		//Check to make sure our UID origin is from the right place
-		if ($row['uid_origin'] == $cfg['val']) {
+		if ($row && $row['uid_origin'] == $cfg['val']) {
 			//If origin is correct, return message count
 			$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($row['username']) . "' AND `archived` = 0");
 			$msgcount = mysqli_num_rows($result);
