6c0ee92c2ef376d9710d3529e8b4dfa8adbe0f4f
TechGeek01 <techgeek01help@gmail.com>
2015-07-27 19:00:26 -0500

    Updated message counter for new system

--- messaging/count-messages.php
+++ messaging/count-messages.php
@@ -6,12 +6,13 @@ require 'functions.php';
 if ($_GET['to'] && $_GET['token']) {
 	$userToken = htmlspecialchars($_GET['token']);
 	$userid = htmlspecialchars($_GET['to']);
-	$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($userid) . "' AND `archived` = 0");
-	$msgcount = mysqli_num_rows($result);
 	$result = mysqli_query($db, "SELECT * FROM `users` WHERE (ext_token = '" . sqlesc($userToken) . "' AND userid = '" . sqlesc($userid) . "') LIMIT 1");
 	$row = mysqli_fetch_array($result);
 
 	if ($row) {
+		$username = mysqli_fetch_assoc($result);
+		$result = mysqli_query($db, "SELECT * FROM `msglist` WHERE `to` = '" . sqlesc($username['username']) . "' AND `archived` = 0");
+		$msgcount = mysqli_num_rows($result);
 		echo $msgcount;
 	} else {
 		echo "Incorrect token";
