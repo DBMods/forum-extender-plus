922d4ed8fa849b76e4df9484206f232dcfb3bda0
TechGeek01 <techgeek01help@gmail.com>
2015-07-27 22:09:01 -0500

    Added forwarding display in archive

--- messaging/archive.php
+++ messaging/archive.php
@@ -12,8 +12,11 @@ if ($userAuthenticated) {
 		echo '<h2>Archived Messages - ' . $archCount . '</h2>';
 		while ($row = mysqli_fetch_assoc($archive)) {
 			echo '<p class="topline">';
-			echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: ' . htmlentities($row['from']) . '<br>Message:<br>' . nl2br(htmlspecialchars($row['msg'])); //PHP 5.4 bug with htmlspecialchars()
-			echo '</p>';
+			echo '<br>Time: ' . gmdate('Y-m-d g:i A', $row['time'] - $timeOffsetSeconds) . '<br>From: ' . htmlentities($row['from']); //PHP 5.4 bug with htmlspecialchars()
+			if (htmlentities($row['forward']) != 0) { //PHP 5.4 bug with htmlspecialchars()
+				echo ' (FWD ' . htmlentities($row['forward']) . ')'; //PHP 5.4 bug with htmlspecialchars()
+			}
+			echo '<br>Message:<br>' . nl2br(htmlspecialchars($row['msg'])) . '</p>';
 			msgOptions($row, "unarch");
 		}
 		deleteConfirm();
