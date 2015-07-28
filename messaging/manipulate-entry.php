6c7f19580efa8fde7c88bcbe544f9d739f0c5c7c
TechGeek01 <techgeek01help@gmail.com>
2015-07-27 21:52:26 -0500

    Updated entry manipulation for new system

--- messaging/manipulate-entry.php
+++ messaging/manipulate-entry.php
@@ -1,12 +1,12 @@
 <?php
 if ($action == 'delete') {
-	$result = mysqli_query($db, "DELETE FROM `msglist` WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($userid) . "'");
+	$result = mysqli_query($db, "DELETE FROM `msglist` WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($username) . "'");
 	echo '<div class="alert-center">' . ($result ? '<div id="alert-fade" class="alert alert-warning"><p><strong>Message deleted.</strong></p></div>' : '<div class="alert alert-danger"><p><strong>Error: Message does not exist or you do not have permission</strong></p></div>') . '</div>';
 } elseif ($action == 'arch') {
-	$result = mysqli_query($db, "UPDATE `msglist` SET `archived` = 1 WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($userid) . "' AND `archived` = 0");
+	$result = mysqli_query($db, "UPDATE `msglist` SET `archived` = 1 WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($username) . "' AND `archived` = 0");
 	echo '<div class="alert-center"><div id="alert-fade" class="alert alert-success"><p><strong>Message archived.</strong></p></div></div>';
 } elseif ($action == 'unarch') {
-	$result = mysqli_query($db, "UPDATE `msglist` SET `archived` = 0 WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($userid) . "' AND `archived` = 1");
+	$result = mysqli_query($db, "UPDATE `msglist` SET `archived` = 0 WHERE `id` = '" . sqlesc($_POST['msgid']) . "' AND `to` = '" . sqlesc($username) . "' AND `archived` = 1");
 	echo '<div class="alert-center"><div id="alert-fade" class="alert alert-success"><p><strong>Message unarchived.</strong></p></div></div>';
 }
 ?>
