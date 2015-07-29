649fa53c9d3744acd3360870f4df81f80b8d9dbb
Andy Yasger <techgeek01help@gmail.com>
2015-07-29 13:52:16 -0500

    Bugfix
    Apparently, I don't use PHP often enough to remember concatenation is a .

--- messaging/fix-uid.php
+++ messaging/fix-uid.php
@@ -17,10 +17,10 @@ if ($row) {
 	$cfg = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `config` WHERE setting = 'default_uid_origin' LIMIT 1"));
 	$result = mysqli_query($db, "UPDATE users SET userid='" . sqlesc($uid) . "', uid_origin='" . $cfg['val'] . "' WHERE (ext_token = '" . sqlesc($userToken) . "' AND username = '" . sqlesc($username) . "') LIMIT 1");
 
-	echo 'Success! Your user ID was updated successfully. You may now click <a href="' + $returnto + '">here</a> to return to the forums.';
+	echo 'Success! Your user ID was updated successfully. You may now click <a href="' . $returnto . '">here</a> to return to the forums.';
 } else {
-	//Updating the UId failed, so alert the user
-	echo 'We had some trouble updating your user ID. Sorry about that. Click <a href="' + $returnto + '">here</a> to return to the forums, and try again later. If the problem persists, click the link in the footer to report a problem.';
+	//Updating the UID failed, so alert the user - Either token or username didn't match records
+	echo 'We had some trouble updating your user ID. Sorry about that. Click <a href="' . $returnto . '">here</a> to return to the forums, and try again later. If the problem persists, click the link in the footer to report a problem.';
 }
 
 echo '</p>';
