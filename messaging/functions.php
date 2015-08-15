6ca7a5d806b25cd795632f4970b2297cde665ab1
TechGeek01 <techgeek01help@gmail.com>
2015-08-15 13:32:26 -0500

    Globalized sign in form

--- messaging/functions.php
+++ messaging/functions.php
@@ -19,6 +19,34 @@ function delCookie($cookie) {
 	}
 }
 
+//Show a sign in panel
+function signinPanel($showOption, $addAction) {
+	//$showOption can show the Register form or a login form to redirect back to the forums with msgtoken
+	echo '<div class="small-center">';
+	echo '<div class="panel panel-primary">';
+	echo '<div class="panel-heading"><h3>Sign in</h3></div>';
+	echo '<div class="panel-body">';
+	echo '<form method="post" action="">';
+	echo '<fieldset>';
+	echo '<div class="form-group"><input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group"><input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" /></div>';
+	echo '<div class="form-group">';
+	echo $addAction ? ("<button name=\"action\" value=\"" . $addAction . "\" class=\"btn btn-success\">Sign in</button>") : ("<button class=\"btn btn-success\">Sign in</button>");
+	echo '</div>';
+	echo '</fieldset>';
+	echo '</form>';
+	if ($showOption == "showRegister")
+		echo "<p>Not registered? <form method='post' action=''><button name='action' class='btn btn-success' value='register'>Sign up!</button></form></p>";
+	if ($showOption == "showTokenRedir")
+		echo "<p>Sign in to allow the extension to access your messaging account</p>";
+	echo '</div>';
+	echo '</div>';
+	echo '</div>';
+}
+function badAuth() {
+	echo "<div class='alert-center'><div id='alert-fade' class='alert alert-danger'><p><strong>Wrong username or password</strong></p></div></div>";
+}
+
 //Append a link to the navbar
 function linkActivity($string) {
 	global $pageName, $userAuthenticated, $showinbox;
--- messaging/sign-in.php
+++ messaging/sign-in.php
@@ -1,30 +1,4 @@
 <?php
-function signinPanel($showOption, $addAction) {
-	//$showOption can show the Register form or a login form to redirect back to the forums with msgtoken
-	echo '<div class="small-center">';
-	echo '<div class="panel panel-primary">';
-	echo '<div class="panel-heading"><h3>Sign in</h3></div>';
-	echo '<div class="panel-body">';
-	echo '<form method="post" action="">';
-	echo '<fieldset>';
-	echo '<div class="form-group"><input id="username" name="username" type="text" placeholder="Username" class="form-control input-md" required="" /></div>';
-	echo '<div class="form-group"><input id="password" name="password" type="password" placeholder="Password" class="form-control input-md" required="" /></div>';
-	echo '<div class="form-group">';
-	echo $addAction ? ("<button name=\"action\" value=\"" . $addAction . "\" class=\"btn btn-success\">Sign in</button>") : ("<button class=\"btn btn-success\">Sign in</button>");
-	echo '</div>';
-	echo '</fieldset>';
-	echo '</form>';
-	if ($showOption == "showRegister")
-		echo "<p>Not registered? <form method='post' action=''><button name='action' class='btn btn-success' value='register'>Sign up!</button></form></p>";
-	if ($showOption == "showTokenRedir")
-		echo "<p>Sign in to allow the extension to access your messaging account</p>";
-	echo '</div>';
-	echo '</div>';
-	echo '</div>';
-}
-function badAuth() {
-	echo "<div class='alert-center'><div id='alert-fade' class='alert alert-danger'><p><strong>Wrong username or password</strong></p></div></div>";
-}
 if ($action == "register" || $action == "create-account" || $action == "pass-token") //clicked register button
 	include "register.php";
 if (!$userAuthenticated && $action != "register" && $action != "create-account" && $action != "pass-token") { //If authentication unsuccessful or not tried
