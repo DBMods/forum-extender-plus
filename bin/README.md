0b9984e610373c0c4d48a43b2e883f4d277aa212
TechGeek01 <techgeek01help@gmail.com>
2015-11-26 17:36:57 -0600

    Added helpList.js scraper

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+#Important notice
+This folder contains various external requirements and dependencies from the main script. The folder itself isn't crucial to the execution of the script. However, the files within it are.
---
+++ bin/helpListScraper.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name Dropbox Forum Extender+ helpList.js Scraper
+// @namespace DropboxMods
+// @description Scrapes data for helpList.js
+// @include https://www.dropboxforum.com/hc/scrapeHelpList/*
+// @include https://www.dropboxforum.com/hc/scrapeHelpList
+// @version 1.0.0
+// @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
+
+/*
+ * This script isn't required as part of the main script
+ * However, this script is used to generate an updated copy of the Help Center
+ * article list, as stored in helpList.js
+ */
+
+window.onload = function() {
+  $('body').html('Checking <span id="check">*starting*</span><br><br>var helpList = {<span id="list"></span><br>};');
+
+  var i, l;
+
+  for (i = 0, l = 12000; i < l; i++) {
+    getEntry(i + 1);
+  }
+}
+
+/*
+ * functions
+ */
+
+function getEntry(num) {
+  $('#check').html(num);
+  GM_xmlhttpRequest({
+    method: 'GET',
+    synchronous: true,
+    url: 'https://www.dropbox.com/help/' + num,
+    onload: function(response) {
+      var resp = response.responseText;
+      if (resp) {
+        var title = resp.split('</title>')[0].split('<title>')[1];
+        if (title != 'Dropbox - 404') {
+          $('#list').append('<br>' + num + ': "' + title.split(' (Dropbox Help Center)')[0] + '",');
+        }
+      }
+      $('#check').html('*idle*');
+    }
+  });
+}
