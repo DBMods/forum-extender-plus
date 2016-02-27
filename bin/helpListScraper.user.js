ee24b63754dd02e615a5a8d784eecda52be4bd32
TechGeek01 <techgeek01help@gmail.com>
2016-02-27 17:14:57 -0600

    Incremented version number
    Raymond* and** I*** never**** really***** learn******, do*******, we********?
    
    * 43852b0
    ** 15a6c18
    *** b5c1db7
    **** ca38983
    ***** 71d8436
    ****** 385c03f
    ******* 82b7b14
    ******** 3eb94cf

--- bin/helpListScraper.user.js
+++ bin/helpListScraper.user.js
@@ -4,7 +4,7 @@
 // @description Scrapes data for helpList.js
 // @include https://www.dropboxforum.com/hc/scrapeHelpList/*
 // @include https://www.dropboxforum.com/hc/scrapeHelpList
-// @version 1.0.5
+// @version 1.0.6
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @downloadURL https://github.com/DBMods/forum-extender-plus/raw/master/bin/helpListScraper.user.js
 // @updateURL https://github.com/DBMods/forum-extender-plus/raw/master/bin/helpListScraper.user.js
