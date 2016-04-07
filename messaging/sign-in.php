55f6f5771942523303ec1978edf940a595ff4186
TechGeek01 <techgeek01help@gmail.com>
2016-04-07 06:34:46 -0500

    Formatting

--- messaging/sign-in.php
+++ messaging/sign-in.php
@@ -4,13 +4,13 @@ if ($action == "register" || $action == "create-account" || $action == "pass-tok
 if (!$userAuthenticated && $action != "register" && $action != "create-account" && $action != "pass-token") { //If authentication unsuccessful or not tried
 	if ($badCookie) {
 		//When user changes password (and therefore token) the token cookie will be outdated
-		//TODO if token cookie is replaced with a session cookie, then we won't need this
+		//TODO If token cookie is replaced with a session cookie, then we won't need this
 		signinPanel();
-	}elseif ($badAuth) {
+	} elseif ($badAuth) {
 		badAuth();
 		signinPanel("showRegister");
-	}else {
-	signinPanel("showRegister");
+	} else {
+		signinPanel("showRegister");
 	}
 }
 ?>
