9b2acc9cd5ee72efb0a5573bebbca9d986b30eb0
Andy Yasger <Andy Yasger>
2016-08-31 10:01:33 -0500

    Help list scraper now works with the main script
    This way, it doesn't overwrite the whole page, and things like the nav bar are still accessible

--- bin/utils/helpListScraper.user.js
+++ bin/utils/helpListScraper.user.js
@@ -2,9 +2,10 @@
 // @name Dropbox Forum Extender+ helpList.js Scraper
 // @namespace DropboxMods
 // @description Scrapes data for helpList.js
-// @include https://www.dropboxforum.com/hc/scrapeHelpList/*
-// @include https://www.dropboxforum.com/hc/scrapeHelpList
-// @version 1.1.3
+// @include https://www.dropboxforum.com/*
+// @exclude https://www.dropboxforum.com/hc/admin/*
+// @exclude https://www.dropboxforum.com/hc/user_avatars/*
+// @version 1.2.0
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @downloadURL https://github.com/DBMods/forum-extender-plus/raw/master/bin/utils/helpListScraper.user.js
 // @updateURL https://github.com/DBMods/forum-extender-plus/raw/master/bin/utils/helpListScraper.user.js
@@ -15,9 +16,14 @@
  * This script isn't required as part of the main script
  * However, this script is used to generate an updated copy of the Help Center
  * article list, as stored in helpList.js
+ *
+ * In order to run this script, you only have to install it, but for full integration
+ * into the main script, it works best if you make sure it's executed after the
+ * main script
  */
 
 var fullUrl = window.location.href;
+var pageUrl = fullUrl.split('https://www.dropboxforum.com/hc/')[1] || '';
 var urlVars = getUrlVars();
 
 var firstItem = true;
@@ -26,9 +32,12 @@ var articleNum = urlVars.start || 1;
 var validEntries = 0;
 var i, l;
 
-window.onload = function() {
+$('#gsDropboxExtenderNav').append('<span><a href="https://www.dropboxforum.com/hc/scrapeHelpList">helpList.js Scraper</a></span>');
+
+if (pageUrl == 'scrapeHelpList') {
   $('title').html('helpList.js Scraper');
-  $('body').html('Checking: <span id="check">*starting*</span><br>Consecutive misses: <span id="miss">0</span><br>Valid articles: <span id="found">0</span><br><br><textarea id="textList" style="width:100%" rows="20"></textarea><span id="list" style="display:none"></span>');
+  $('.error-page').html('<h1>helpList.js Scraper</h1>');
+  $('.error-page').append('<p><span style="font-size:16px;color:#999">Checking: <span id="check" style="color:#000">*starting*</span><br>Consecutive misses: <span id="miss" style="color:#000">0</span>&emsp;&emsp;Valid articles: <span id="found" style="color:#000">0</span></span></p><textarea id="textList" style="width:100%" rows="15" placeholder="Sit tight, we\'re looking for pages"></textarea><span id="list" style="display:none"></span>');
 
   while (missCounter <= 5000) {
     getEntry(articleNum);
